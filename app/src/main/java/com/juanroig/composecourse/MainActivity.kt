59e3985340d4ab45225b71f5d3bcaf5d1a5e93bb
Juan Roig <juan.roig4t@gmail.com>
2023-11-14 12:37:17 +0100

    add card

--- app/src/main/java/com/juanroig/composecourse/MainActivity.kt
+++ app/src/main/java/com/juanroig/composecourse/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.juanroig.composecourse.ui.screen.CardScreen
 import com.juanroig.composecourse.ui.screen.SelectionScreen
 import com.juanroig.composecourse.ui.theme.ComposeCourseTheme
 
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SelectionScreen()
+                    CardScreen()
                 }
             }
         }

4e7bb28b812ab05bf7d74d580356fcc614ca5bf4
Juan Roig <juan.roig4t@gmail.com>
2023-11-14 16:50:14 +0100

    add DividerAndDropDownMenuScreen

--- app/src/main/java/com/juanroig/composecourse/MainActivity.kt
+++ app/src/main/java/com/juanroig/composecourse/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.juanroig.composecourse.ui.screen.ScaffoldScreen
+import com.juanroig.composecourse.ui.screen.DividerAndDropDownMenuScreen
 import com.juanroig.composecourse.ui.theme.ComposeCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ScaffoldScreen()
+                    DividerAndDropDownMenuScreen()
                 }
             }
         }

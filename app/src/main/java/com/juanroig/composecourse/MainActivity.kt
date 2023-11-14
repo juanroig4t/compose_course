1de35cf29051dfa3d2c0d2d6a25be7ed5cac3479
Juan Roig <juan.roig4t@gmail.com>
2023-11-14 11:33:33 +0100

    add selection components

--- app/src/main/java/com/juanroig/composecourse/MainActivity.kt
+++ app/src/main/java/com/juanroig/composecourse/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.juanroig.composecourse.ui.screen.ImageScreen
+import com.juanroig.composecourse.ui.screen.SelectionScreen
 import com.juanroig.composecourse.ui.theme.ComposeCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ImageScreen()
+                    SelectionScreen()
                 }
             }
         }

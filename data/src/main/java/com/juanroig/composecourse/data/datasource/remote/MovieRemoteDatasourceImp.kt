e8022ba9bce57871f5abb245e13cc5e3d87560aa
Juan Roig <juan.roig4t@gmail.com>
2024-05-30 16:35:03 +0200

    Fake response

--- data/src/main/java/com/juanroig/composecourse/data/datasource/remote/MovieRemoteDatasourceImp.kt
+++ data/src/main/java/com/juanroig/composecourse/data/datasource/remote/MovieRemoteDatasourceImp.kt
@@ -1,11 +1,20 @@
 package com.juanroig.composecourse.data.datasource.remote
 
+import android.content.Context
+import com.google.gson.Gson
 import com.juanroig.composecourse.data.datasource.MovieRemoteDatasource
+import com.juanroig.composecourse.data.datasource.remote.model.MovieDto
 import com.juanroig.composecourse.data.mapper.toDomain
+import com.juanroig.composecourse.domain.model.core.error.CustomFailure
 import com.juanroig.composecourse.domain.model.core.error.NetworkFailure
 import com.juanroig.composecourse.domain.model.core.result.Result
 import com.juanroig.composecourse.domain.model.movie.Movie
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.delay
+import okio.use
 import retrofit2.HttpException
+import java.io.BufferedReader
+import kotlin.random.Random
 
 class MovieRemoteDatasourceImp(
     private val retrofitMovieNetworkApi: RetrofitMovieNetworkApi
@@ -23,3 +32,34 @@ class MovieRemoteDatasourceImp(
         }
     }
 }
+
+class FakeMovieRemoteDatasourceImp(
+    private val retrofitMovieNetworkApi: RetrofitMovieNetworkApi,
+    @ApplicationContext private val context: Context,
+) : MovieRemoteDatasource {
+
+    companion object {
+        private const val MOVIES_ASSET = "movieList.json"
+    }
+    override suspend fun getPopularMovies(): Result<List<Movie>> {
+        context.assets.open(MOVIES_ASSET).use {
+            delay(Random.nextLong(1000, 5000))
+            val content = it.bufferedReader().use(BufferedReader::readText)
+            val movieResponse = Gson().fromJson(content, MovieResponse::class.java)
+            movieResponse.results?.let { movies ->
+                return Result.Success(
+                    movies.map {
+                        it.toDomain()
+                    }
+                )
+            } ?: return Result.Error(CustomFailure.NoResponse)
+        }
+    }
+}
+
+data class MovieResponse(
+    val page: Int,
+    val results: List<MovieDto>?,
+    val total_pages: Int,
+    val total_results: Int
+)
--- data/src/main/java/com/juanroig/composecourse/data/di/NetworkModule.kt
+++ data/src/main/java/com/juanroig/composecourse/data/di/NetworkModule.kt
@@ -1,13 +1,16 @@
 package com.juanroig.composecourse.data.di
 
+import android.content.Context
 import com.juanroig.composecourse.data.BuildConfig
 import com.juanroig.composecourse.data.datasource.MovieRemoteDatasource
+import com.juanroig.composecourse.data.datasource.remote.FakeMovieRemoteDatasourceImp
 import com.juanroig.composecourse.data.datasource.remote.MovieRemoteDatasourceImp
 import com.juanroig.composecourse.data.datasource.remote.RetrofitMovieNetworkApi
 import com.juanroig.composecourse.data.datasource.remote.util.BasicAuthInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.ResponseBody
@@ -25,9 +28,10 @@ internal object NetworkModule {
     @Provides
     @Singleton
     fun provideRemoteDataSource(
-        retrofitMovieNetworkApi: RetrofitMovieNetworkApi
+        retrofitMovieNetworkApi: RetrofitMovieNetworkApi,
+        @ApplicationContext context: Context
     ): MovieRemoteDatasource {
-        return MovieRemoteDatasourceImp(retrofitMovieNetworkApi)
+        return FakeMovieRemoteDatasourceImp(retrofitMovieNetworkApi, context)
     }
 
     @Provides
